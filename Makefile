b76391fd54623728241448616e55050c596c4270
OverPoweredDev <omkar.prabhune19@vit.edu>
2020-09-04 12:53:55 +0530

    stuff

--- Makefile
+++ Makefile
@@ -1,19 +1,15 @@
 GPP = g++
-SFML_VERSION=2.5.1
+SFML_VERSION = 2.5.1
 
-SFML_LIB=/usr/local/lib/SFML-$(SFML_VERSION)/lib
-SFML_HEADERS=/usr/local/lib/SFML-$(SFML_VERSION)/include
+SFML_LIB = /usr/local/lib/SFML-$(SFML_VERSION)/lib
+SFML_HEADERS = /usr/local/lib/SFML-$(SFML_VERSION)/include
 
-FLAGS = -std=c++11 -Wall -L $(SFML_LIB) -I $(SFML_HEADERS)
-LIBS= -lsfml-graphics -lsfml-window -lsfml-system
+FLAGS = -Wall -L $(SFML_LIB) -I $(SFML_HEADERS)
+LIBS = -lsfml-graphics -lsfml-window -lsfml-system
 
-# Final binary
-BIN = editor
+BIN = loop
 
-# Put all auto generated stuff to this build dir.
-BUILD_DIR = ./build
-
-# List of all .cpp source files.
-# CPP = main.cpp $(wildcard dir1/*.cpp) $(wildcard dir2/*.cpp)
-CPP = Editor.cpp $(wildcard src/*.cpp)
-
+.PHONY: all loop clean
+loop:
+	$(GPP) $(FLAGS) -o $(BIN) editor.cpp $(LIBS)
+	./$(BIN)
--- editor.cpp
+++ editor.cpp
@@ -1,8 +1,22 @@
+#include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
+#include <SFML/System.hpp>
 
-int main()
-{
-    sf::Window window;
-    window.create(sf::VideoMode(800, 600), "My window");
-    return 0;
+int main() {
+    sf::RenderWindow window(sf::VideoMode(720, 405), "loop");
+    window.setVerticalSyncEnabled(true);
+    sf::Color backgroundColor = sf::Color(255, 29, 45);
+    window.clear(backgroundColor);
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
 }
